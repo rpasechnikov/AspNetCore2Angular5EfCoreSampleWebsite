5f11c886d33ee07223c3765e2a9a27c2e0bf97d3
Rostislav Pasechnikov <pasechnikov_ros@hotmail.com>
2018-03-14 10:44:45 -0700

    Minor renames and cleanup to make the example clearer

---
+++ AspNetCoreAngularSite/ClientApp/gallery/thumbnail.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'thumbnail',
+    templateUrl: "./thumbnail.component.html",
+    styles: []
+})
+export class Thumbnail {
+    title = 'Thumbnail Component';
+}
